41155fce4ffab94929f1ccda21f425c3fc5cc0e5
xinyi <xinyi.17@intl.zju.edu.cn>
2020-04-10 17:30:47 -0500

    cleaned, layout done

--- Final/FinalSetCalendar/app/src/main/java/com/example/finalsetcalendar/DetectActivity.java
+++ Final/FinalSetCalendar/app/src/main/java/com/example/finalsetcalendar/DetectActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.ByteArrayOutputStream;
 
 public class DetectActivity extends AppCompatActivity {
 
