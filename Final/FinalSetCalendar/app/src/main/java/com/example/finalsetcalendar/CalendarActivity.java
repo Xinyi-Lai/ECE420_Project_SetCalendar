43735ab86d70b369b1c4ad8856d130b7e3338172
xinyi <xinyi.17@intl.zju.edu.cn>
2020-04-30 16:31:42 -0500

    set calendar in the background

--- Final/FinalSetCalendar/app/src/main/java/com/example/finalsetcalendar/CalendarActivity.java
+++ Final/FinalSetCalendar/app/src/main/java/com/example/finalsetcalendar/CalendarActivity.java
@@ -1,6 +1,8 @@
 package com.example.finalsetcalendar;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
@@ -44,7 +46,6 @@ public class CalendarActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -58,7 +59,8 @@ public class CalendarActivity extends AppCompatActivity {
         }
         // Permission granted, add event
         if (checkSelfPermission == PackageManager.PERMISSION_GRANTED) {
-            addCalendarEvent(getApplicationContext(), "Title", "description");
+            addCalendarEvent(getApplicationContext(), "313", "final");
+            //CalendarQuery();
             return;
         } else {
             // Permission not granted, ask for permission
@@ -74,7 +76,8 @@ public class CalendarActivity extends AppCompatActivity {
         if (requestCode == CALENDAR_REQUEST_CODE) {
             // Permission granted, add event
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                addCalendarEvent(getApplicationContext(), "Title", "description");
+                addCalendarEvent(getApplicationContext(), "313", "LAB");
+                //CalendarQuery();
             } else {
                 Toast.makeText(this, "Calendar Permission is Required", Toast.LENGTH_SHORT).show();
             }
@@ -82,38 +85,65 @@ public class CalendarActivity extends AppCompatActivity {
     }
 
 
+    private static String CALENDARS_NAME = "localname";
+    private static String CALENDARS_ACCOUNT_NAME = "localaccount@gmail.com";
+    private static String CALENDARS_ACCOUNT_TYPE = CalendarContract.ACCOUNT_TYPE_LOCAL;
+    private static String CALENDARS_DISPLAY_NAME = "localdisplay";
 
-//    // Projection array. Creating indices for this array instead of doing
-//    // dynamic lookups improves performance.
-//    public static final String[] EVENT_PROJECTION = new String[] {
-//            CalendarContract.Calendars._ID,                           // 0
-//            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-//            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-//            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
-//    };
-//    // The indices for the projection array above.
-//    private static final int PROJECTION_ID_INDEX = 0;
-//    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-//    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-//    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-//
-//
-//
-//    // Run query
-//    Cursor cur = null;
-//    ContentResolver cr = getContentResolver();
-//    Uri uri = Calendars.CONTENT_URI;
-//    String selection = "((" + Calendars.ACCOUNT_NAME + " = ?) AND ("
-//            + Calendars.ACCOUNT_TYPE + " = ?) AND ("
-//            + Calendars.OWNER_ACCOUNT + " = ?))";
-//    String[] selectionArgs = new String[] {"hera@example.com", "com.example",
-//            "hera@example.com"};
-//// Submit the query and get a Cursor object back.
-//    cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+    // Projection array. Creating indices for this array instead of doing
+    // dynamic lookups improves performance.
+    public static final String[] EVENT_PROJECTION = new String[] {
+            CalendarContract.Calendars._ID,                           // 0
+            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
+            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
+            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
+    };
+    // The indices for the projection array above.
+    private static final int PROJECTION_ID_INDEX = 0;
+    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
+    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
+    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
 
 
+    @SuppressLint("MissingPermission")
+    private void CalendarQuery() {
+        Log.d("CAL", "in CalendarQuery");
 
-    private static String CALANDER_URL = "content://com.android.calendar/calendars";
+        // Run query
+        Cursor cur = null;
+        ContentResolver cr = getContentResolver();
+        Uri uri = CalendarContract.Calendars.CONTENT_URI;
+        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
+                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
+                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
+        String[] selectionArgs = new String[] {"xlai7@illinois.edu", "com.google", "xlai7@illinois.edu"};
+        //String[] selectionArgs = new String[] {"xinyi0lai@gmail.com", "com.google", "xinyi0lai@gmail.com"};
+
+        // Submit the query and get a Cursor object back.
+        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
+
+        // Use the cursor to step through the returned records
+        while (cur.moveToNext()) {
+
+            long calID = 0;
+            String displayName = null;
+            String accountName = null;
+            String ownerName = null;
+
+            // Get the field values
+            calID = cur.getLong(PROJECTION_ID_INDEX);
+            displayName = cur.getString(PROJECTION_DISPLAY_NAME_INDEX);
+            accountName = cur.getString(PROJECTION_ACCOUNT_NAME_INDEX);
+            ownerName = cur.getString(PROJECTION_OWNER_ACCOUNT_INDEX);
+            Log.d("CAL", "calID:"+calID+"; displayName:"+displayName+"; accountName:"+accountName+"; ownerName:"+ownerName);
+
+        }
+
+    }
+
+
+
+    private static String CALANDER_URL = "content://com.android.calendar/calendars";        // CalendarContract.Calendars.CONTENT_URI;
     private static String CALANDER_EVENT_URL = "content://com.android.calendar/events";
 
     private static int checkCalendarAccount(Context context) {
@@ -135,10 +165,7 @@ public class CalendarActivity extends AppCompatActivity {
         }
     }
 
-    private static String CALENDARS_NAME = "localname";
-    private static String CALENDARS_ACCOUNT_NAME = "localaccount@gmail.com";
-    private static String CALENDARS_ACCOUNT_TYPE = CalendarContract.ACCOUNT_TYPE_LOCAL;
-    private static String CALENDARS_DISPLAY_NAME = "localdisplay";
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     private static long addCalendarAccount(Context context) {
@@ -187,14 +214,10 @@ public class CalendarActivity extends AppCompatActivity {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public static void addCalendarEvent(Context context, String title, String description){
-        // 获取日历账户的id
-        int calId = checkAndAddCalendarAccount(context);
-        if (calId < 0) {
-            // 获取账户id失败直接返回，添加日历事件失败
-            Log.d("tag", "calID<0");
-            return;
-        }
+        // Direct to my calendar account
+        int calId = 4;
         Log.d("tag", "adding calID:"+calId);
+
         ContentValues event = new ContentValues();
         event.put("title", title);
         event.put("description", description);
@@ -204,10 +227,10 @@ public class CalendarActivity extends AppCompatActivity {
         long startMillis = 0;
         long endMills = 0;
         Calendar beginTime = Calendar.getInstance();
-        beginTime.set(2020,4,29,7,30);
+        beginTime.set(2020,3,29,9,30);
         startMillis = beginTime.getTimeInMillis();
         Calendar endTime = Calendar.getInstance();
-        endTime.set(2020,4,29,8,30);
+        endTime.set(2020,3,29,10,30);
         endMills = endTime.getTimeInMillis();
 
         event.put(CalendarContract.Events.DTSTART, startMillis);
@@ -253,125 +276,5 @@ public class CalendarActivity extends AppCompatActivity {
         }
     }
 
-//    public static void readCalendarEvent() {
-//        Toast.makeText(this, "In Query", Toast.LENGTH_SHORT).show();
-//        // Run query
-//        Cursor cur = null;
-//        ContentResolver cr = getContentResolver();
-//        Uri uri = CalendarContract.Calendars.CONTENT_URI;
-//        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-//                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
-//                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-//        String[] selectionArgs = new String[]{"hera@example.com", "com.example", "hera@example.com"};
-//        // Submit the query and get a Cursor object back.
-//        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-//        Log.d("tag", "while");
-//        while (cur.moveToNext()) {
-//            Log.d("tag", "in while");
-//            if (cur!=null){
-//                int id_1 = cur.getColumnIndex(CalendarContract.Events._ID);
-//                int id_2 = cur.getColumnIndex(CalendarContract.Events.TITLE);
-//                int id_3 = cur.getColumnIndex(CalendarContract.Events.DESCRIPTION);
-//                int id_4 = cur.getColumnIndex(CalendarContract.Events.EVENT_LOCATION);
-//
-//                String idValue = cur.getColumnName(id_1);
-//                String titleValue = cur.getString(id_2);
-//                String descriptionValue = cur.getString((id_3));
-//                String eventValue = cur.getString(id_4);
-//
-//                Toast.makeText(this, idValue + ", " + titleValue + ", " + descriptionValue + ", " + eventValue, Toast.LENGTH_SHORT).show();
-//    }
-
-
-//    // Projection array. Creating indices for this array instead of doing
-//    // dynamic lookups improves performance.
-//    public static final String[] EVENT_PROJECTION = new String[]{
-//            CalendarContract.Calendars._ID,                           // 0
-//            CalendarContract.Calendars.ACCOUNT_NAME,                  // 1
-//            CalendarContract.Calendars.CALENDAR_DISPLAY_NAME,         // 2
-//            CalendarContract.Calendars.OWNER_ACCOUNT                  // 3
-//    };
-//
-//    // The indices for the projection array above.
-//    private static final int PROJECTION_ID_INDEX = 0;
-//    private static final int PROJECTION_ACCOUNT_NAME_INDEX = 1;
-//    private static final int PROJECTION_DISPLAY_NAME_INDEX = 2;
-//    private static final int PROJECTION_OWNER_ACCOUNT_INDEX = 3;
-
-
-
-
-
-
-
-
-//    @RequiresApi(api = Build.VERSION_CODES.N)
-//    private void askCalendarPermissions() {
-//        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CALENDAR}, CALENDAR_READ_PERM_CODE);
-//        }
-//        else if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_CALENDAR) != PackageManager.PERMISSION_GRANTED){
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_CALENDAR}, CALENDAR_WRITE_PERM_CODE);
-//        }
-//        else {
-//            addCalendarEvent(getApplicationContext(), "Title", "description");
-//        }
-//    }
-
-//    @RequiresApi(api = Build.VERSION_CODES.N)
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        if (true) {
-//            if (grantResults.length < 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(CalendarActivity.this, "Here", Toast.LENGTH_SHORT).show();
-//                addCalendarEvent(getApplicationContext(), "Title", "description");
-//            } else {
-//                Toast.makeText(CalendarActivity.this, "Camera Permission is Required", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-
-//    private void CalendarQuery() {
-//        Toast.makeText(CalendarActivity.this, "In Query", Toast.LENGTH_SHORT).show();
-//        // Run query
-//        Cursor cur = null;
-//        ContentResolver cr = getContentResolver();
-//        Uri uri = CalendarContract.Calendars.CONTENT_URI;
-//        String selection = "((" + CalendarContract.Calendars.ACCOUNT_NAME + " = ?) AND ("
-//                + CalendarContract.Calendars.ACCOUNT_TYPE + " = ?) AND ("
-//                + CalendarContract.Calendars.OWNER_ACCOUNT + " = ?))";
-//        String[] selectionArgs = new String[]{"hera@example.com", "com.example", "hera@example.com"};
-//        // Submit the query and get a Cursor object back.
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CALENDAR) != PackageManager.PERMISSION_GRANTED) {
-//            // TODO: Consider calling
-//            //    ActivityCompat#requestPermissions
-//            // here to request the missing permissions, and then overriding
-//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//            //                                          int[] grantResults)
-//            // to handle the case where the user grants the permission. See the documentation
-//            // for ActivityCompat#requestPermissions for more details.
-//            return;
-//        }
-//        cur = cr.query(uri, EVENT_PROJECTION, selection, selectionArgs, null);
-//        Log.d("tag", "while");
-//        while (cur.moveToNext()) {
-//            Log.d("tag", "in while");
-//            if (cur!=null){
-//                int id_1 = cur.getColumnIndex(CalendarContract.Events._ID);
-//                int id_2 = cur.getColumnIndex(CalendarContract.Events.TITLE);
-//                int id_3 = cur.getColumnIndex(CalendarContract.Events.DESCRIPTION);
-//                int id_4 = cur.getColumnIndex(CalendarContract.Events.EVENT_LOCATION);
-//
-//                String idValue = cur.getColumnName(id_1);
-//                String titleValue = cur.getString(id_2);
-//                String descriptionValue = cur.getString((id_3));
-//                String eventValue = cur.getString(id_4);
-//
-//                Toast.makeText(this, idValue + ", " + titleValue + ", " + descriptionValue + ", " + eventValue, Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(this, "Event is not present", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
 
 }
